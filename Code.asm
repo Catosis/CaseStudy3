81d575e58e33c85935fb5aa5710c6f9418d216e2
yaoshawn <yaoshawn@users.noreply.github.com>
2018-03-01 17:51:18 -0500

    Check

--- Code.asm
+++ Code.asm
@@ -1,9 +1,23 @@
 LIST P=16F747
-title "A/D Demo Program"
+title "Solenoid Control"
 
 #include <P16F747.INC> ; include file for the device
 
 __CONFIG _CONFIG1, _FOSC_HS & _CP_OFF & _DEBUG_OFF & _VBOR_2_0 & _BOREN_0 & _MCLR_ON & _PWRTE_ON & _WDT_OFF
 __CONFIG _CONFIG2, _BORSEN_0 & _IESO_OFF & _FCMEN_OFF
 
-; new line
+	org 00h
+	goto initPort
+
+	org 04h
+	goto isrService
+
+	org 10h
+
+;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+;
+; Port Initialization
+
+initPort
+	
+	

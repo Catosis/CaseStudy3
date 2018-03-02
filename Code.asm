ae75f52908d8acd6c34c9fe3d100815aba5ce93f
Connie <Exodus@Exodus.local>
2018-03-01 19:08:14 -0500

    Mode1 additions

--- Code.asm
+++ Code.asm
@@ -8,6 +8,9 @@ __CONFIG _CONFIG2, _BORSEN_0 & _IESO_OFF & _FCMEN_OFF
 
 ;Variable Declarations
 	
+	mode equ 20h
+	modeComf equ 21h
+	
 	org 00h
 	goto initPort
 
@@ -21,14 +24,34 @@ __CONFIG _CONFIG2, _BORSEN_0 & _IESO_OFF & _FCMEN_OFF
 ; Port Initialization
 
     initPort
-	clrf PORTE	; Clear Port E output latches
+	clrf PORTE	; Clear Port E input latches (Modes)
+	clrf PORTC	; Clear Port C input latches (red = 1, green = 0)
+	clrf PORTB	; LED lights
+	clrf PORTD	; Clear Port D Solenoid output latches + sensor output
 	bsf STATUS,RP0 ; Set bit in STATUS register for bank 1
 	movlw B'11111111' ; move hex value FF into W register (maybe no need)
-	movwf TRISC ; Configure Port C as all inputs
-
+	movwf TRISE ; Configure Port E as all inputs
+	movwf TRISC ; Configure port C as all inputs
+	movlw B'00111111' ; move hex value FF into W register (maybe no need)
+	movwf TRISD ; Configure D for input and output
+	clrf  TRISB ; Config B as all outputs
+	
 	bcf STATUS,RP0 ; Clear bit in STATUS register for bank 0
 	
-    GreenPress
+    waitPress ;sees if green button is pressed, goto Green Press
+	btfsc PORTC,0 ; see if green button pressed
+	goto GreenPress ; green button is pressed - goto routine
+	goto waitPress ; keep checking
+    
+    GreenPress ;read in switch, go to correct mode
+	movf PORTE,W ;(hopefully) moves values in port E to register W
+	movwf modeComf
+	comf modeComf, mode
+	if mode==B'11011111'
+	end
 
-    Mode1RedPress
+    Mode1RedPress ;activate solenoid on red press
+	btfss PORTC,1 ; see if red button pressed
+	goto Mode1RedPress
+	; actual response for solenoid
     
